35fc21785e5d3d49ad364dbe310d37dff9e4ab9c
Ganzitokiler <126096942+Ganzitokiler@users.noreply.github.com>
2023-05-07 22:05:10 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ventana frame = new Ventana("Layouts");
+
+        VentanaGrid gridFrame = new VentanaGrid("GridLayout");
+    }
+}
